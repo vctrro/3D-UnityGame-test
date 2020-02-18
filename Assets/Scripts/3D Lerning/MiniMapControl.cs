1676468e74a6dce10f5f24f186882bf48de726a2
Victor Ro <vctr.ro@gmail.com>
2020-02-19 00:43:03 +0400

    MiniMap done. Visual styling

---
+++ Assets/Scripts/3D Lerning/MiniMapControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapControl : MonoBehaviour
+{
+    private Transform _player;
+    private void Start()
+    {
+        _player = GameObject.Find("Player").transform;
+        transform.up = _player.forward;
+    }
+
+    void Update()
+    {
+        transform.rotation = _player.rotation;
+    }
+}
